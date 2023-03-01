a804dd9da993a5d03895116456c5bf6fecdb9b74
Angel Cerecero <91641441+KETTZO@users.noreply.github.com>
2023-03-01 15:29:35 -0600

    PROYECTO

---
+++ POI PROYECTO/src/pages/Login.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+//import add from "../img/a4.png";
+import './Login_Style.css';
+import logo3 from '../img/logo3.png'
+
+const Login = () => {
+    const [err, setErr] = useState(false);
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+
+        //const auth = getAuth();
+        try{
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate("/");
+        }catch(err){
+            setErr(true);
+        }
+    };
+
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <img src={logo3} alt="" />
+                <span id='title' className="title">Inicio de sesión</span>
+                <form onSubmit={handleSubmit}>
+                    <input id="typeEmail" type="email" placeholder="Correo"/>
+                    <input id='typePassword' type="password" placeholder="Contraseña"/>
+                    <button id='join'>Entrar</button>
+                    {err && <span>Pasó algo malo...</span>}
+                </form>
+                <p>¿No tienes Cuenta? <Link to="/register">Registrate</Link></p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ POI PROYECTO/src/pages/Register.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import add from "../img/a4.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
+import './Register_Style.css';
+import logo3 from '../img/logo3.png'
+import logo4 from '../img/logo4.png'
+
+
+const Register = () => {
+    const [err, setErr] = useState(false);
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;    
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+
+        //const auth = getAuth();
+        try{
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+            //const storage = getStorage();
+            const storageRef = ref(storage, displayName);
+
+            /*const uploadTask = await uploadBytesResumable(storageRef, file);
+
+            uploadTask.on(
+            (error) => {
+                setErr(true);
+            },*/
+            await uploadBytesResumable(storageRef, file).then(() => {
+                //getDownloadURL(uploadTask.snapshot.ref).then( async (downloadURL) => {
+                getDownloadURL(storageRef).then(async (downloadURL) => {
+                    await updateProfile(res.user, {
+                        displayName,
+                        photoURL: downloadURL,
+                    });
+                    await setDoc(doc(db, "users", res.user.uid),{
+                        uid: res.user.uid,
+                        displayName,
+                        email,
+                        photoURL : downloadURL
+                    });
+
+                    await setDoc(doc(db, "userChats", res.user.uid), {});
+                    navigate("/");
+
+                });
+            }
+            );
+        }catch(err){
+            setErr(true);
+        }
+    };
+
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <img src={logo3} alt="" />
+                <span id="title" className="title">Registro</span>
+                <form onSubmit={handleSubmit}>
+                    <input id="name" type="text" placeholder="Nombre"/>
+                    <input id="correo" type="email" placeholder="Correo"/>
+                    <input id="contra" type="password" placeholder="Contraseña"/>
+                    <input style={{display: "none"}} type="file" id="File"/>
+                    <label  htmlFor="File">  
+                        <img id="logo" src={logo4} alt="" />
+                        <span id='foto'>Agregar foto</span>
+                    </label>
+                    <select id="carrera">
+                        <option disabled selected value="">Seleccione una carrera</option>
+                        <option value="">LMAD</option>
+                        <option value="">LCC</option>
+                        <option value="">LM</option>
+                        <option value="">LSTI</option>
+                        <option value="">LF</option>
+                    </select>
+                    <button id="join">Registrarse</button>
+                    {err && <span>Paso algo malo...</span>}
+                </form>
+                <p>¿Ya tienes Cuenta? <Link to="/login">Ingresar</Link></p>
+            </div>
+        </div>
+    )
+}
+
+export default Register
